040c3fe2585d6fb887e09d16222149bd9cdc213a
BenMartFilio <ecobmartinezfilio@gmail.com>
2025-11-04 08:59:29 +0100

    [BMF] habillage

--- Assets/Scripts/Element/PushCube.cs
+++ Assets/Scripts/Element/PushCube.cs
@@ -8,7 +8,7 @@ public class PushCube : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotation; 
+        rb.constraints = RigidbodyConstraints.FreezeRotation; //éviter que le cube tourne dans tous les sens
     }
 
     private void OnCollisionStay(Collision collision)
@@ -19,7 +19,7 @@ public class PushCube : MonoBehaviour
             return;
         }
 
-        Vector3 pousseeDirection = new Vector3(collision.contacts[0].normal.x, 0, collision.contacts[0].normal.z);
+        Vector3 pousseeDirection = new Vector3(collision.contacts[0].normal.x, 0, collision.contacts[0].normal.z); //prend la direction par laquelle le joueur pousse le cube (il prend la normal au point de contact des deux plans)
         rb.AddForce(-pousseeDirection * forcePoussee, ForceMode.Force);
     }
 }
